2cba2c01e5adda20ac95fba20cd6f474d167d6bf
yxb <Yuxuebing201034@163.com>
2018-06-26 10:27:29 +0800

    spring boot2.0动态数据源切换

--- ShareModel/src/test/java/pers/yxb/share/sharemodel/ShareModelApplicationTests.java
+++ ShareModel/src/test/java/pers/yxb/share/sharemodel/ShareModelApplicationTests.java
@@ -1,5 +1,6 @@
 package pers.yxb.share.sharemodel;
 
+import org.hibernate.annotations.Synchronize;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +10,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ShareModelApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	public static void main(String[] args) {
+		synchronized (Synchronize.class){
+			System.out.println("Synchronize");
+		}
 	}
 
 }
